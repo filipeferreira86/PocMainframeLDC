78032b5e97403fb304b76f68a6b13cc303f992f8
filipe.jesus <filipe.jesus@gx2-13.gx2.local>
2019-01-11 14:18:12 -0200

    Criação classe runner

--- src/test/java/runners/RunnerTest.java
+++ src/test/java/runners/RunnerTest.java
@@ -1,5 +1,22 @@
 package runners;
 
-public class RunnerTest {
+import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		snippets = SnippetType.CAMELCASE,
+		plugin = {"pretty", 
+				"html:target/cucumber-html-report",
+				"json:target/cucumber.json", 
+				"junit:target/cucumber.xml",
+				"rerun:target/rerun.txt"},
+		features = "src/test/resources/features/MainFramelogin.feature",
+		glue = "steps"
+		)
+
+public class RunnerTest {
 }
